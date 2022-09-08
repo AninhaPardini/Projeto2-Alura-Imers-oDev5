335a5ca782b4858e70fdfc1417778cf4290a0571
Ana Luiza Pardini <aninhapardini.work@gmail.com>
2022-09-07 23:56:02 -0300

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function Convert() {
     
     const StringValue = document.getElementById('coinvalue').value;
     const Value = parseFloat(StringValue);
-    const FinallyValue = (Value * 5) + 285.83;
+    const FinallyValue = (Value + 285.83 * 5).toFixed(2)
     const result = document.getElementById('result');
     console.log(FinallyValue);
 
